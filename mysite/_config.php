c1dc8b63695b2d4cea8e7257584e701f46f442ea
Lloyd Emelle <lloyd@emelle.me>
2012-04-01 16:14:06 +0000

    Update to config file

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,17 +3,35 @@
 global $project;
 $project = 'mysite';
 
-global $database;
-$database = 'septa';
+$dbname = (getenv("OPENSHIFT_DB_USERNAME") != FALSE) ? $_ENV['OPENSHIFT_DB_USERNAME'] : "septa";
+//global $database;
+//$database = $dbname;
 
 // Use _ss_environment.php file for configuration
-require_once("conf/ConfigureFromEnv.php");
+//require_once("conf/ConfigureFromEnv.php");
 
+/* What kind of environment is this: development, test, or live (ie, production */
+Director::set_environment_type("dev");
+
+$server = (getenv('OPENSHIFT_DB_HOST') !== FALSE) ? $_ENV['OPENSHIFT_DB_HOST'] : "localhost";
+$username = (getenv("OPENSHIFT_DB_USERNAME") != FALSE) ? $_ENV['OPENSHIFT_DB_USERNAME'] : "root";
+$password = (getenv("OPENSHIFT_DB_PASSWORD") != FALSE) ? $_ENV['OPENSHIFT_DB_PASSWORD'] : "";
+
+global $databaseConfig;
+$databaseConfig = array(
+        "type" => 'MySQLDatabase',
+        "server" => $server,
+        "username" => $username,
+        "password" => $password,
+        "database" => $dbname,
+        "path" => '',
+);
 
 MySQLDatabase::set_connection_charset('utf8');
-
-/* URL Rules for Map View */
+/* URL Rules for Bus View */
 Director::addRules(50, array('bus/$Action/$ID/$OtherID' => 'Page_Controller'));
+/* URL Rules for Train View */
+Director::addRules(50, array('bus/$Action/$ID/$OtherID' => 'Train_Controller'));
 
 // This line set's the current theme. More themes can be
 // downloaded from http://www.silverstripe.org/themes/
